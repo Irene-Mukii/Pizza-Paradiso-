85d3cfd2741b16d46b3042ee212dc07db35c47c4
Irene Mukii <renniequin@gmail.com>
2025-03-19 01:10:03 +0300

    edits url hardcoded so as to allow vercel deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export const getServerSideProps = async (ctx) => {
   if(myCookie.token === process.env.NEXT_PUBLIC_TOKEN){
     admin = true
   }
-  const res = await axios.get('http://localhost:3000/api/products');
+  const res = await axios.get(`${process.env.NEXT_PUBLIC_URL}api/products`);
   return {
     props: {
       pizzaList: res.data,
